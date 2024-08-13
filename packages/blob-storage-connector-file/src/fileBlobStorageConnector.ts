20f3bbf481b4bab43aa67ce022e31ab2b96bb59c
Martyn Janes <martyn@obany.com>
2024-08-13 10:11:04 +0200

    Update to new bootstrap method signature

--- packages/blob-storage-connector-file/src/fileBlobStorageConnector.ts
+++ packages/blob-storage-connector-file/src/fileBlobStorageConnector.ts
@@ -51,9 +51,9 @@ export class FileBlobStorageConnector implements IBlobStorageConnector {
 	/**
 	 * Bootstrap the connector by creating and initializing any resources it needs.
 	 * @param nodeLoggingConnectorType The node logging connector type, defaults to "node-logging".
-	 * @returns The response of the bootstrapping as log entries.
+	 * @returns True if the bootstrapping process was successful.
 	 */
-	public async bootstrap(nodeLoggingConnectorType?: string): Promise<void> {
+	public async bootstrap(nodeLoggingConnectorType?: string): Promise<boolean> {
 		const nodeLogging = LoggingConnectorFactory.getIfExists(
 			nodeLoggingConnectorType ?? "node-logging"
 		);
@@ -89,6 +89,7 @@ export class FileBlobStorageConnector implements IBlobStorageConnector {
 					},
 					error: BaseError.fromError(err)
 				});
+				return false;
 			}
 		} else {
 			await nodeLogging?.log({
@@ -100,6 +101,8 @@ export class FileBlobStorageConnector implements IBlobStorageConnector {
 				}
 			});
 		}
+
+		return true;
 	}
 
 	/**
