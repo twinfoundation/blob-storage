4f6d579c01b3ae13ebcd9029b279da62e4fde859
Cornel Cristian Filip <cornel.filip@iota.org>
2024-10-11 15:52:57 +0300

    feat: Add GCP blob storage connector (#8)
    * feat: Add GCP blob storage connector
    
    * npm i
    
    * add scheme flag to docker
    
    * refactor
    
    * add GCP as a step
    
    * use GCP as a service
    
    * add http
    
    * run GCP docker as a step
    
    * add TODO
    
    * start GCP emulator as a step
    
    * My Signed Commit
    
    Signed-off-by: Cornel <cornel.filip@iota.org>
    
    ---------
    
    Signed-off-by: Cornel <cornel.filip@iota.org>

---
+++ packages/blob-storage-connector-gcp/tests/setupTestEnv.ts
@@ -0,0 +1,51 @@
+// Copyright 2024 IOTA Stiftung.
+// SPDX-License-Identifier: Apache-2.0.
+import path from "node:path";
+import { Storage } from "@google-cloud/storage";
+import { Guards } from "@twin.org/core";
+import * as dotenv from "dotenv";
+import type { IGcpBlobStorageConnectorConfig } from "../src/models/IGcpBlobStorageConnectorConfig";
+
+dotenv.config({ path: [path.join(__dirname, ".env"), path.join(__dirname, ".env.dev")] });
+
+console.debug("Setting up test environment from .env and .env.dev files");
+
+Guards.stringValue("TestEnv", "TEST_GCP_PROJECT_ID", process.env.TEST_GCP_PROJECT_ID);
+Guards.stringValue("TestEnv", "TEST_GCP_BUCKET", process.env.TEST_GCP_BUCKET);
+Guards.stringValue("TestEnv", "TEST_GCP_API_ENDPOINT", process.env.TEST_GCP_API_ENDPOINT);
+
+export const TEST_GCP_CONFIG: IGcpBlobStorageConnectorConfig = {
+	projectId: process.env.TEST_GCP_PROJECT_ID,
+	bucketName: process.env.TEST_GCP_BUCKET,
+	apiEndpoint: process.env.TEST_GCP_API_ENDPOINT,
+	protocol: "http"
+};
+
+/**
+ * Create a test bucket for the tests.
+ */
+export async function createTestBucket(): Promise<void> {
+	const storage = new Storage({
+		projectId: TEST_GCP_CONFIG.projectId,
+		apiEndpoint: TEST_GCP_CONFIG.apiEndpoint
+	});
+
+	console.log(`Attempting to connect to GCP Storage endpoint '${TEST_GCP_CONFIG.apiEndpoint}'`);
+
+	try {
+		const [buckets] = await storage.getBuckets();
+		console.log("Successfully connected to GCP Storage endpoint.");
+		const bucketExists = buckets.some(bucket => bucket.name === TEST_GCP_CONFIG.bucketName);
+
+		if (bucketExists) {
+			console.log(`Test bucket '${TEST_GCP_CONFIG.bucketName}' already exists.`);
+			return;
+		}
+
+		await storage.createBucket(TEST_GCP_CONFIG.bucketName);
+		console.log(`Test bucket '${TEST_GCP_CONFIG.bucketName}' created successfully.`);
+	} catch (error) {
+		console.error("Error creating test bucket:", error);
+		throw error;
+	}
+}
